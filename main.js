cd7c819029e954b45820a7c7fbac1e263d6c2730
TariqAlQadi <tariqalqadi@gmx.de>
2023-01-12 22:48:43 +0100

    finish java & localstorage

---
+++ main.js
@@ -0,0 +1,24 @@
+const colorThemes = document.querySelectorAll('[name="theme"]');
+
+//store theme
+const storeTheme = function (theme) {
+  localStorage.setItem("theme", theme);
+};
+
+//apply theme
+const retrieveTheme = function () {
+  const activeTheme = localStorage.getItem("theme");
+  colorThemes.forEach((themeOption) => {
+    if (themeOption.id === activeTheme) {
+      themeOption.checked = true;
+    }
+  });
+};
+
+colorThemes.forEach((themeOption) => {
+  themeOption.addEventListener("click", () => {
+    storeTheme(themeOption.id);
+  });
+});
+
+document.onload = retrieveTheme();
